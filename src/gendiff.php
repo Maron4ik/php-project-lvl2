029675644241c97a0b048ca14acc8840e9f947e8
alexeymaron <maron.alexey@gmail.com>
2021-10-09 07:15:58 +0300

    cooding

---
+++ src/gendiff.php
@@ -0,0 +1,16 @@
+<?php
+
+$help = <<<DOC
+Generate diff
+
+Usage:
+  gendiff (-h|--help)
+  gendiff (-v|--version)
+
+Options:
+  -h --help                     Show this screen
+  -v --version                  Show version
+
+DOC;
+
+echo $help;
--- test.php
+++ test.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 function getText($test)
 {
     echo $test;
